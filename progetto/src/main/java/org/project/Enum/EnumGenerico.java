354ec6ff84536f770d7a92e6980a41428f53b151
MentallyPink <155743431+MentallyPink@users.noreply.github.com>
2024-01-09 16:00:07 +0100

    13 commit: aggiunto enum nomi bot

---
+++ progetto/src/main/java/org/project/Enum/EnumGenerico.java
@@ -0,0 +1,9 @@
+package org.project.Enum;
+
+public enum EnumGenerico {
+	Razor,  Viper ,  Shadow ,  Ghost ,  Spike ,  Reaper ,  Snake ,  Scar ,  Bones ,  Wolf ,
+     Joker ,  Savage ,  Raven ,  Blade ,  Cobra ,  Ace ,  Diesel ,  Rogue ,  Killer ,  Maverick ,
+     Mamba ,  Raptor ,  Bullet ,  Havoc ,  Vandal ,  Bandit ,  Sinner ,  Fury ,  Gunner ,  Crash ,
+     Skull ,  Rage ,  Venom ,  Thrasher ,  Wraith ,  Talon ,  Grizzly ,  Slick ,
+     Sledge ,  Ruckus ,  Bruiser ,  Slayer ,  Trigger ,  Outlaw ,  Blitz ,  Torch ,  Stitch ,  Psycho 
+}
